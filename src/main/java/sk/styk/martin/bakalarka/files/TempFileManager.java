67f18c47faf751b37eb4bdf44f66693c0f0f715d
MartinStyk <martin.styk@gmail.com>
2015-11-29 15:25:35 +0100

    added manager of temp files

---
+++ src/main/java/sk/styk/martin/bakalarka/files/TempFileManager.java
@@ -0,0 +1,97 @@
+package sk.styk.martin.bakalarka.files;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Martin Styk on 29.11.2015.
+ */
+public class TempFileManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(TempFileManager.class);
+    File decompiledDir;
+    File unzipDir;
+    File workingDir;
+    private String defaultTempDirBase = "D:\\Projects\\temp";
+    private File apk;
+
+    public TempFileManager(File apk) {
+        if (apk == null) {
+            throw new IllegalArgumentException("apk");
+        }
+        this.apk = apk;
+    }
+
+    public static TempFileManager forApk(File apk) {
+        return new TempFileManager(apk);
+    }
+
+    public String getDefaultTempFileBaseDir() {
+        return defaultTempDirBase;
+    }
+
+    public void setDefaultTempFileBaseDir(String defaultTempFileBaseDir) {
+        defaultTempDirBase = defaultTempFileBaseDir;
+    }
+
+    public File getApkWorkingDirectory() {
+        return workingDir == null ? createApkWorkingDirectory() : workingDir;
+    }
+
+    public File createApkWorkingDirectory() {
+        File newDir = new File(defaultTempDirBase + File.separator + apk.getName());
+        if (!newDir.exists()) {
+            logger.trace("creating working dir " + newDir.getAbsolutePath());
+            newDir.mkdirs();
+        }
+        return newDir;
+    }
+
+    public File getApkUnzipedDirectory() {
+        return workingDir == null ? createApkUnzipedDirectory() : unzipDir;
+    }
+
+    public File createApkUnzipedDirectory() {
+        workingDir = getApkWorkingDirectory();
+        unzipDir = new File(workingDir, "unziped");
+        if (!unzipDir.exists()) {
+            logger.trace("creating unzip dir " + unzipDir.getAbsolutePath());
+            unzipDir.mkdirs();
+        }
+        return unzipDir;
+    }
+
+    public File getApkDecompiledDirectory() {
+        return decompiledDir == null ? createApkDecompiledDirectory() : decompiledDir;
+    }
+
+    public File createApkDecompiledDirectory() {
+        workingDir = getApkWorkingDirectory();
+        decompiledDir = new File(workingDir, "decompiled");
+        if (!decompiledDir.exists()) {
+            logger.trace("creating decompiled dir " + decompiledDir.getAbsolutePath());
+            decompiledDir.mkdirs();
+        }
+        return decompiledDir;
+    }
+
+    public void deleteApkDecompiledDirectory() throws IOException {
+        FileUtils.deleteDirectory(decompiledDir);
+    }
+
+    public void deleteApkUnzipedDirectory() throws IOException {
+        FileUtils.deleteDirectory(unzipDir);
+    }
+
+    public void deleteApkWorkingDirectory() throws IOException {
+        deleteApkDecompiledDirectory();
+        deleteApkUnzipedDirectory();
+        FileUtils.deleteDirectory(workingDir);
+
+    }
+
+
+}
