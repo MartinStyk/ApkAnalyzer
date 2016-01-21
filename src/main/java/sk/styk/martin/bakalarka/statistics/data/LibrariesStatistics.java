068786eb573f5878aa8dd1f2790fd29663f470d6
MartinStyk <martin.styk@gmail.com>
2016-01-21 17:27:02 +0100

    Libraries statistics processing

--- src/main/java/sk/styk/martin/bakalarka/statistics/data/LibrariesStatistics.java
+++ src/main/java/sk/styk/martin/bakalarka/statistics/data/LibrariesStatistics.java
@@ -1,5 +1,7 @@
 package sk.styk.martin.bakalarka.statistics.data;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -7,79 +9,177 @@ import java.util.Map;
  */
 public class LibrariesStatistics {
 
+    /**
+     * number of apks with data successfully collected from android manifest
+     */
+    private Integer analyzedApks;
+    /**
+     * number of apks with libraries tag found in manifest
+     */
+
     //libraries
-    private Integer numberOfApksWithLibrariesObtained;
-    private Integer numberOfLibrariesArithmeticMean;
-    private Integer numberOfLibrariesModus;
-    private Integer numberOLibrariesMedian;
-    private Map<String,Integer> topLibraries;
+    private Integer numberOfApksWithLibrariesTagObtained;
+    private BigDecimal librariesArithmeticMean;
+    private List<Integer> librariesModus;
+    private Integer librariesMedian;
+    private Integer librariesMax;
+    private Integer librariesMin;
+    private BigDecimal librariesVariance;
+    private BigDecimal librariesDeviation;
 
-    private Integer numberOfLibrariesArithmeticMeanNonZero;
-    private Integer numberOfLibrariesModusNonZero;
-    private Integer numberOLibrariesMedianNonZero;
+    private Integer numberOfApksWithLibrariesTagObtainedNonZero;
+    private BigDecimal librariesArithmeticMeanNonZero;
+    private List<Integer> librariesModusNonZero;
+    private Integer librariesMedianNonZero;
+    private Integer librariesMaxNonZero;
+    private Integer librariesMinNonZero;
+    private BigDecimal librariesVarianceNonZero;
+    private BigDecimal librariesDeviationNonZero;
 
-    public Integer getNumberOfApksWithLibrariesObtained() {
-        return numberOfApksWithLibrariesObtained;
+    private Map<String, Pair<Integer, BigDecimal>> topLibraries;
+
+    public Integer getNumberOfApksWithLibrariesTagObtained() {
+        return numberOfApksWithLibrariesTagObtained;
     }
 
-    public void setNumberOfApksWithLibrariesObtained(Integer numberOfApksWithLibrariesObtained) {
-        this.numberOfApksWithLibrariesObtained = numberOfApksWithLibrariesObtained;
+    public void setNumberOfApksWithLibrariesTagObtained(Integer numberOfApksWithLibrariesTagObtained) {
+        this.numberOfApksWithLibrariesTagObtained = numberOfApksWithLibrariesTagObtained;
     }
 
-    public Integer getNumberOfLibrariesArithmeticMean() {
-        return numberOfLibrariesArithmeticMean;
+    public BigDecimal getLibrariesArithmeticMean() {
+        return librariesArithmeticMean;
     }
 
-    public void setNumberOfLibrariesArithmeticMean(Integer numberOfLibrariesArithmeticMean) {
-        this.numberOfLibrariesArithmeticMean = numberOfLibrariesArithmeticMean;
+    public void setLibrariesArithmeticMean(BigDecimal librariesArithmeticMean) {
+        this.librariesArithmeticMean = librariesArithmeticMean;
     }
 
-    public Integer getNumberOfLibrariesModus() {
-        return numberOfLibrariesModus;
+    public List<Integer> getLibrariesModus() {
+        return librariesModus;
     }
 
-    public void setNumberOfLibrariesModus(Integer numberOfLibrariesModus) {
-        this.numberOfLibrariesModus = numberOfLibrariesModus;
+    public void setLibrariesModus(List<Integer> librariesModus) {
+        this.librariesModus = librariesModus;
     }
 
-    public Integer getNumberOLibrariesMedian() {
-        return numberOLibrariesMedian;
+    public Integer getLibrariesMedian() {
+        return librariesMedian;
     }
 
-    public void setNumberOLibrariesMedian(Integer numberOLibrariesMedian) {
-        this.numberOLibrariesMedian = numberOLibrariesMedian;
+    public void setLibrariesMedian(Integer librariesMedian) {
+        this.librariesMedian = librariesMedian;
     }
 
-    public Map<String, Integer> getTopLibraries() {
+    public Integer getLibrariesMax() {
+        return librariesMax;
+    }
+
+    public void setLibrariesMax(Integer librariesMax) {
+        this.librariesMax = librariesMax;
+    }
+
+    public Integer getLibrariesMin() {
+        return librariesMin;
+    }
+
+    public void setLibrariesMin(Integer librariesMin) {
+        this.librariesMin = librariesMin;
+    }
+
+    public BigDecimal getLibrariesVariance() {
+        return librariesVariance;
+    }
+
+    public void setLibrariesVariance(BigDecimal librariesVariance) {
+        this.librariesVariance = librariesVariance;
+    }
+
+    public BigDecimal getLibrariesDeviation() {
+        return librariesDeviation;
+    }
+
+    public void setLibrariesDeviation(BigDecimal librariesDeviation) {
+        this.librariesDeviation = librariesDeviation;
+    }
+
+    public Map<String, Pair<Integer, BigDecimal>> getTopLibraries() {
         return topLibraries;
     }
 
-    public void setTopLibraries(Map<String, Integer> topLibraries) {
+    public void setTopLibraries(Map<String, Pair<Integer, BigDecimal>> topLibraries) {
         this.topLibraries = topLibraries;
     }
 
-    public Integer getNumberOfLibrariesArithmeticMeanNonZero() {
-        return numberOfLibrariesArithmeticMeanNonZero;
+    public Integer getNumberOfApksWithLibrariesTagObtainedNonZero() {
+        return numberOfApksWithLibrariesTagObtainedNonZero;
     }
 
-    public void setNumberOfLibrariesArithmeticMeanNonZero(Integer numberOfLibrariesArithmeticMeanNonZero) {
-        this.numberOfLibrariesArithmeticMeanNonZero = numberOfLibrariesArithmeticMeanNonZero;
+    public void setNumberOfApksWithLibrariesTagObtainedNonZero(Integer numberOfApksWithLibrariesTagObtainedNonZero) {
+        this.numberOfApksWithLibrariesTagObtainedNonZero = numberOfApksWithLibrariesTagObtainedNonZero;
     }
 
-    public Integer getNumberOfLibrariesModusNonZero() {
-        return numberOfLibrariesModusNonZero;
+    public BigDecimal getLibrariesArithmeticMeanNonZero() {
+        return librariesArithmeticMeanNonZero;
     }
 
-    public void setNumberOfLibrariesModusNonZero(Integer numberOfLibrariesModusNonZero) {
-        this.numberOfLibrariesModusNonZero = numberOfLibrariesModusNonZero;
+    public void setLibrariesArithmeticMeanNonZero(BigDecimal librariesArithmeticMeanNonZero) {
+        this.librariesArithmeticMeanNonZero = librariesArithmeticMeanNonZero;
     }
 
-    public Integer getNumberOLibrariesMedianNonZero() {
-        return numberOLibrariesMedianNonZero;
+    public List<Integer> getLibrariesModusNonZero() {
+        return librariesModusNonZero;
     }
 
-    public void setNumberOLibrariesMedianNonZero(Integer numberOLibrariesMedianNonZero) {
-        this.numberOLibrariesMedianNonZero = numberOLibrariesMedianNonZero;
+    public void setLibrariesModusNonZero(List<Integer> librariesModusNonZero) {
+        this.librariesModusNonZero = librariesModusNonZero;
+    }
+
+    public Integer getLibrariesMedianNonZero() {
+        return librariesMedianNonZero;
+    }
+
+    public void setLibrariesMedianNonZero(Integer librariesMedianNonZero) {
+        this.librariesMedianNonZero = librariesMedianNonZero;
+    }
+
+    public Integer getLibrariesMaxNonZero() {
+        return librariesMaxNonZero;
+    }
+
+    public void setLibrariesMaxNonZero(Integer librariesMaxNonZero) {
+        this.librariesMaxNonZero = librariesMaxNonZero;
+    }
+
+    public Integer getLibrariesMinNonZero() {
+        return librariesMinNonZero;
+    }
+
+    public void setLibrariesMinNonZero(Integer librariesMinNonZero) {
+        this.librariesMinNonZero = librariesMinNonZero;
+    }
+
+    public BigDecimal getLibrariesVarianceNonZero() {
+        return librariesVarianceNonZero;
+    }
+
+    public void setLibrariesVarianceNonZero(BigDecimal librariesVarianceNonZero) {
+        this.librariesVarianceNonZero = librariesVarianceNonZero;
+    }
+
+    public BigDecimal getLibrariesDeviationNonZero() {
+        return librariesDeviationNonZero;
+    }
+
+    public void setLibrariesDeviationNonZero(BigDecimal librariesDeviationNonZero) {
+        this.librariesDeviationNonZero = librariesDeviationNonZero;
+    }
+
+    public Integer getAnalyzedApks() {
+        return analyzedApks;
+    }
+
+    public void setAnalyzedApks(Integer analyzedApks) {
+        this.analyzedApks = analyzedApks;
     }
 
     @Override
@@ -89,47 +189,85 @@ public class LibrariesStatistics {
 
         LibrariesStatistics that = (LibrariesStatistics) o;
 
-        if (numberOfApksWithLibrariesObtained != null ? !numberOfApksWithLibrariesObtained.equals(that.numberOfApksWithLibrariesObtained) : that.numberOfApksWithLibrariesObtained != null)
+        if (analyzedApks != null ? !analyzedApks.equals(that.analyzedApks) : that.analyzedApks != null) return false;
+        if (numberOfApksWithLibrariesTagObtained != null ? !numberOfApksWithLibrariesTagObtained.equals(that.numberOfApksWithLibrariesTagObtained) : that.numberOfApksWithLibrariesTagObtained != null)
             return false;
-        if (numberOfLibrariesArithmeticMean != null ? !numberOfLibrariesArithmeticMean.equals(that.numberOfLibrariesArithmeticMean) : that.numberOfLibrariesArithmeticMean != null)
+        if (librariesArithmeticMean != null ? !librariesArithmeticMean.equals(that.librariesArithmeticMean) : that.librariesArithmeticMean != null)
             return false;
-        if (numberOfLibrariesModus != null ? !numberOfLibrariesModus.equals(that.numberOfLibrariesModus) : that.numberOfLibrariesModus != null)
+        if (librariesModus != null ? !librariesModus.equals(that.librariesModus) : that.librariesModus != null)
             return false;
-        if (numberOLibrariesMedian != null ? !numberOLibrariesMedian.equals(that.numberOLibrariesMedian) : that.numberOLibrariesMedian != null)
+        if (librariesMedian != null ? !librariesMedian.equals(that.librariesMedian) : that.librariesMedian != null)
             return false;
-        if (topLibraries != null ? !topLibraries.equals(that.topLibraries) : that.topLibraries != null) return false;
-        if (numberOfLibrariesArithmeticMeanNonZero != null ? !numberOfLibrariesArithmeticMeanNonZero.equals(that.numberOfLibrariesArithmeticMeanNonZero) : that.numberOfLibrariesArithmeticMeanNonZero != null)
+        if (librariesMax != null ? !librariesMax.equals(that.librariesMax) : that.librariesMax != null) return false;
+        if (librariesMin != null ? !librariesMin.equals(that.librariesMin) : that.librariesMin != null) return false;
+        if (librariesVariance != null ? !librariesVariance.equals(that.librariesVariance) : that.librariesVariance != null)
             return false;
-        if (numberOfLibrariesModusNonZero != null ? !numberOfLibrariesModusNonZero.equals(that.numberOfLibrariesModusNonZero) : that.numberOfLibrariesModusNonZero != null)
+        if (librariesDeviation != null ? !librariesDeviation.equals(that.librariesDeviation) : that.librariesDeviation != null)
             return false;
-        return !(numberOLibrariesMedianNonZero != null ? !numberOLibrariesMedianNonZero.equals(that.numberOLibrariesMedianNonZero) : that.numberOLibrariesMedianNonZero != null);
+        if (numberOfApksWithLibrariesTagObtainedNonZero != null ? !numberOfApksWithLibrariesTagObtainedNonZero.equals(that.numberOfApksWithLibrariesTagObtainedNonZero) : that.numberOfApksWithLibrariesTagObtainedNonZero != null)
+            return false;
+        if (librariesArithmeticMeanNonZero != null ? !librariesArithmeticMeanNonZero.equals(that.librariesArithmeticMeanNonZero) : that.librariesArithmeticMeanNonZero != null)
+            return false;
+        if (librariesModusNonZero != null ? !librariesModusNonZero.equals(that.librariesModusNonZero) : that.librariesModusNonZero != null)
+            return false;
+        if (librariesMedianNonZero != null ? !librariesMedianNonZero.equals(that.librariesMedianNonZero) : that.librariesMedianNonZero != null)
+            return false;
+        if (librariesMaxNonZero != null ? !librariesMaxNonZero.equals(that.librariesMaxNonZero) : that.librariesMaxNonZero != null)
+            return false;
+        if (librariesMinNonZero != null ? !librariesMinNonZero.equals(that.librariesMinNonZero) : that.librariesMinNonZero != null)
+            return false;
+        if (librariesVarianceNonZero != null ? !librariesVarianceNonZero.equals(that.librariesVarianceNonZero) : that.librariesVarianceNonZero != null)
+            return false;
+        if (librariesDeviationNonZero != null ? !librariesDeviationNonZero.equals(that.librariesDeviationNonZero) : that.librariesDeviationNonZero != null)
+            return false;
+        return !(topLibraries != null ? !topLibraries.equals(that.topLibraries) : that.topLibraries != null);
 
     }
 
     @Override
     public int hashCode() {
-        int result = numberOfApksWithLibrariesObtained != null ? numberOfApksWithLibrariesObtained.hashCode() : 0;
-        result = 31 * result + (numberOfLibrariesArithmeticMean != null ? numberOfLibrariesArithmeticMean.hashCode() : 0);
-        result = 31 * result + (numberOfLibrariesModus != null ? numberOfLibrariesModus.hashCode() : 0);
-        result = 31 * result + (numberOLibrariesMedian != null ? numberOLibrariesMedian.hashCode() : 0);
+        int result = analyzedApks != null ? analyzedApks.hashCode() : 0;
+        result = 31 * result + (numberOfApksWithLibrariesTagObtained != null ? numberOfApksWithLibrariesTagObtained.hashCode() : 0);
+        result = 31 * result + (librariesArithmeticMean != null ? librariesArithmeticMean.hashCode() : 0);
+        result = 31 * result + (librariesModus != null ? librariesModus.hashCode() : 0);
+        result = 31 * result + (librariesMedian != null ? librariesMedian.hashCode() : 0);
+        result = 31 * result + (librariesMax != null ? librariesMax.hashCode() : 0);
+        result = 31 * result + (librariesMin != null ? librariesMin.hashCode() : 0);
+        result = 31 * result + (librariesVariance != null ? librariesVariance.hashCode() : 0);
+        result = 31 * result + (librariesDeviation != null ? librariesDeviation.hashCode() : 0);
+        result = 31 * result + (numberOfApksWithLibrariesTagObtainedNonZero != null ? numberOfApksWithLibrariesTagObtainedNonZero.hashCode() : 0);
+        result = 31 * result + (librariesArithmeticMeanNonZero != null ? librariesArithmeticMeanNonZero.hashCode() : 0);
+        result = 31 * result + (librariesModusNonZero != null ? librariesModusNonZero.hashCode() : 0);
+        result = 31 * result + (librariesMedianNonZero != null ? librariesMedianNonZero.hashCode() : 0);
+        result = 31 * result + (librariesMaxNonZero != null ? librariesMaxNonZero.hashCode() : 0);
+        result = 31 * result + (librariesMinNonZero != null ? librariesMinNonZero.hashCode() : 0);
+        result = 31 * result + (librariesVarianceNonZero != null ? librariesVarianceNonZero.hashCode() : 0);
+        result = 31 * result + (librariesDeviationNonZero != null ? librariesDeviationNonZero.hashCode() : 0);
         result = 31 * result + (topLibraries != null ? topLibraries.hashCode() : 0);
-        result = 31 * result + (numberOfLibrariesArithmeticMeanNonZero != null ? numberOfLibrariesArithmeticMeanNonZero.hashCode() : 0);
-        result = 31 * result + (numberOfLibrariesModusNonZero != null ? numberOfLibrariesModusNonZero.hashCode() : 0);
-        result = 31 * result + (numberOLibrariesMedianNonZero != null ? numberOLibrariesMedianNonZero.hashCode() : 0);
         return result;
     }
 
     @Override
     public String toString() {
         return "LibrariesStatistics{" +
-                "numberOfApksWithLibrariesObtained=" + numberOfApksWithLibrariesObtained +
-                ", numberOfLibrariesArithmeticMean=" + numberOfLibrariesArithmeticMean +
-                ", numberOfLibrariesModus=" + numberOfLibrariesModus +
-                ", numberOLibrariesMedian=" + numberOLibrariesMedian +
+                "analyzedApks=" + analyzedApks +
+                ", numberOfApksWithLibrariesTagObtained=" + numberOfApksWithLibrariesTagObtained +
+                ", librariesArithmeticMean=" + librariesArithmeticMean +
+                ", librariesModus=" + librariesModus +
+                ", librariesMedian=" + librariesMedian +
+                ", librariesMax=" + librariesMax +
+                ", librariesMin=" + librariesMin +
+                ", librariesVariance=" + librariesVariance +
+                ", librariesDeviation=" + librariesDeviation +
+                ", numberOfApksWithLibrariesTagObtainedNonZero=" + numberOfApksWithLibrariesTagObtainedNonZero +
+                ", librariesArithmeticMeanNonZero=" + librariesArithmeticMeanNonZero +
+                ", librariesModusNonZero=" + librariesModusNonZero +
+                ", librariesMedianNonZero=" + librariesMedianNonZero +
+                ", librariesMaxNonZero=" + librariesMaxNonZero +
+                ", librariesMinNonZero=" + librariesMinNonZero +
+                ", librariesVarianceNonZero=" + librariesVarianceNonZero +
+                ", librariesDeviationNonZero=" + librariesDeviationNonZero +
                 ", topLibraries=" + topLibraries +
-                ", numberOfLibrariesArithmeticMeanNonZero=" + numberOfLibrariesArithmeticMeanNonZero +
-                ", numberOfLibrariesModusNonZero=" + numberOfLibrariesModusNonZero +
-                ", numberOLibrariesMedianNonZero=" + numberOLibrariesMedianNonZero +
                 '}';
     }
 }
---
+++ src/main/java/sk/styk/martin/bakalarka/statistics/processors/LibrariesStatisticsProcessor.java
@@ -0,0 +1,170 @@
+package sk.styk.martin.bakalarka.statistics.processors;
+
+import org.apache.commons.math3.stat.StatUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sk.styk.martin.bakalarka.analyze.data.AndroidManifestData;
+import sk.styk.martin.bakalarka.analyze.data.ApkData;
+import sk.styk.martin.bakalarka.files.JsonUtils;
+import sk.styk.martin.bakalarka.statistics.data.Pair;
+import sk.styk.martin.bakalarka.statistics.data.LibrariesStatistics;
+import sk.styk.martin.bakalarka.statistics.processors.helpers.ConversionHelper;
+import sk.styk.martin.bakalarka.statistics.processors.helpers.PercentageHelper;
+import sk.styk.martin.bakalarka.statistics.processors.helpers.SortingHelper;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Martin Styk on 21.01.2016.
+ */
+public class LibrariesStatisticsProcessor {
+    private List<File> jsons;
+    private LibrariesStatistics librariesStatistics;
+    private static final Logger logger = LoggerFactory.getLogger(LibrariesStatisticsProcessor.class);
+
+
+    public LibrariesStatisticsProcessor(List<File> jsons) {
+        if (jsons == null || jsons.isEmpty())
+            throw new IllegalArgumentException("jsons");
+
+        this.jsons = jsons;
+    }
+
+    public static LibrariesStatisticsProcessor ofFiles(List<File> jsons) {
+        return new LibrariesStatisticsProcessor(jsons);
+    }
+
+    public LibrariesStatistics process() {
+
+        librariesStatistics = new LibrariesStatistics();
+        Map<String, Integer> topLibraries = new HashMap<String, Integer>();
+        List<Double> librariesNumbersList = new ArrayList<Double>();
+        List<Double> librariesNumbersListNonZero = new ArrayList<Double>();
+        int manifestFound = 0;
+
+        for (int i = 0; i < jsons.size(); i++) {
+            if (i % StatisticsProcessor.PRINT_MESSAGE_INTERVAL == 0) {
+                logger.info("Loading json number " + i);
+            }
+
+            File f = jsons.get(i);
+            ApkData data = JsonUtils.fromJson(f);
+            AndroidManifestData manifestData = null;
+
+
+            if (data != null && data.getAndroidManifest() != null) {
+
+                manifestFound++;
+                manifestData = data.getAndroidManifest();
+
+                List<String> librariesList = manifestData.getUsesLibrary();
+
+                if (librariesList != null) {
+                    int listSize = librariesList.size();
+                    librariesNumbersList.add(new Double(listSize));
+                    if (listSize != 0) {
+                        librariesNumbersListNonZero.add(new Double(listSize));
+                    }
+
+                    for (String perm : librariesList) {
+                        if (topLibraries.containsKey(perm)) {
+                            Integer oldValue = topLibraries.get(perm);
+                            topLibraries.put(perm, ++oldValue);
+                        } else {
+                            topLibraries.put(perm, 1);
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+
+        setValues(manifestFound, librariesNumbersList, false);
+        setValues(manifestFound, librariesNumbersListNonZero, true);
+        setTopLibraries(topLibraries, librariesNumbersList.size());
+
+        return librariesStatistics;
+    }
+
+    private void setValues(int manifestFound, List<Double> librariesNumbersList, boolean isNonZero) {
+
+        if (librariesNumbersList == null) {
+            throw new NullPointerException("librariesNumbersList null");
+        }
+
+        logger.info("Started processing libraries");
+
+        double[] array = ConversionHelper.toDoubleArray(librariesNumbersList);
+
+        Double mean = StatUtils.mean(array);
+        Double median = StatUtils.percentile(array, 50);
+        double[] modus = StatUtils.mode(array);
+        Double minimum = StatUtils.min(array);
+        Double maximum = StatUtils.max(array);
+        Double variance = StatUtils.variance(array);
+        Double deviation = Math.sqrt(variance);
+
+        if (!isNonZero) {
+
+            librariesStatistics.setAnalyzedApks(manifestFound);
+            librariesStatistics.setNumberOfApksWithLibrariesTagObtained(librariesNumbersList.size());
+
+            librariesStatistics.setLibrariesArithmeticMean(new BigDecimal(mean));
+            librariesStatistics.setLibrariesModus(ConversionHelper.toIntegerList(modus));
+            librariesStatistics.setLibrariesMedian(median.intValue());
+            librariesStatistics.setLibrariesMax(maximum.intValue());
+            librariesStatistics.setLibrariesMin(minimum.intValue());
+            librariesStatistics.setLibrariesVariance(new BigDecimal(variance));
+            librariesStatistics.setLibrariesDeviation(new BigDecimal(deviation));
+
+        } else {
+            librariesStatistics.setNumberOfApksWithLibrariesTagObtainedNonZero(librariesNumbersList.size());
+
+            librariesStatistics.setLibrariesArithmeticMeanNonZero(new BigDecimal(mean));
+            librariesStatistics.setLibrariesModusNonZero(ConversionHelper.toIntegerList(modus));
+            librariesStatistics.setLibrariesMedianNonZero(median.intValue());
+            librariesStatistics.setLibrariesMaxNonZero(maximum.intValue());
+            librariesStatistics.setLibrariesMinNonZero(minimum.intValue());
+            librariesStatistics.setLibrariesVarianceNonZero(new BigDecimal(variance));
+            librariesStatistics.setLibrariesDeviationNonZero(new BigDecimal(deviation));
+        }
+        logger.info("Finished processing libraries");
+    }
+
+    private void setTopLibraries(Map<String, Integer> topLibraries, Integer wholeData) {
+
+        logger.info("Started processing libraries chart");
+
+        if (wholeData == null) {
+            throw new IllegalArgumentException("wholeData");
+        }
+        if (topLibraries == null) {
+            throw new IllegalArgumentException("topLibraries");
+        }
+
+        Map<String, Pair<Integer, BigDecimal>> result = new HashMap<String, Pair<Integer, BigDecimal>>();
+
+        for (Map.Entry<String, Integer> entry : topLibraries.entrySet()) {
+            String name = entry.getKey();
+            Integer number = entry.getValue();
+            BigDecimal percentage = PercentageHelper.getPercentage(number.doubleValue(), wholeData);
+
+            Pair<Integer, BigDecimal> percentageEntry = new Pair<Integer, BigDecimal>(number, percentage);
+
+            result.put(name, percentageEntry);
+        }
+        librariesStatistics.setTopLibraries(SortingHelper.sortByValue(result));
+
+        logger.info("Finished processing libraries chart");
+    }
+
+}
+
+
