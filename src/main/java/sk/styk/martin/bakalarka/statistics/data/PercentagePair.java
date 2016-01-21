04e9cfa24ac0afa83cf0a460f643b856a349cc4f
MartinStyk <martin.styk@gmail.com>
2016-01-21 18:19:25 +0100

    Percentage Pair class

---
+++ src/main/java/sk/styk/martin/bakalarka/statistics/data/PercentagePair.java
@@ -0,0 +1,12 @@
+package sk.styk.martin.bakalarka.statistics.data;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Martin Styk on 21.01.2016.
+ */
+public class PercentagePair extends Pair<Integer, BigDecimal> {
+    public PercentagePair(Integer val, BigDecimal per) {
+        super(val, per);
+    }
+}
